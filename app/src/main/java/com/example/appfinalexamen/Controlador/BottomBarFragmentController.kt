235b491d8dcfa4b61152fd2989ef3de377ca74aa
Gerardo Cabrera <gerardojaviercr@gmail.com>
2020-12-13 19:57:37 -0600

    Fixed Fragment

--- app/src/main/java/com/example/appfinalexamen/Controlador/BottomBarFragmentController.kt
+++ app/src/main/java/com/example/appfinalexamen/Controlador/BottomBarFragmentController.kt
@@ -14,6 +14,7 @@ import androidx.navigation.findNavController
 import com.example.appfinalexamen.Entidades.Pastes
 import com.example.appfinalexamen.Modelo.ImageConverter
 import com.example.appfinalexamen.Modelo.PastesDB
+import com.example.appfinalexamen.PasteCasa
 import com.example.appfinalexamen.R
 import kotlinx.android.synthetic.main.fragment_paste_casa.*
 import kotlinx.android.synthetic.main.item_lista.view.*
@@ -25,7 +26,7 @@ class BottomBarFragmentController : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bottom_bar_fragment_controller)
 
-        LlenarInformacion()
+        //LlenarInformacion()
 
     }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -36,41 +37,4 @@ class BottomBarFragmentController : AppCompatActivity() {
         return true
     }
 
-    fun LlenarInformacion(){
-        val datasource = PastesDB(this)
-
-        val registros = ArrayList<Pastes>()
-
-        val cursor = datasource.consultPastes()
-
-        while (cursor.moveToNext()){
-            val columnas = Pastes(cursor.getInt(0), cursor.getString(1), cursor.getString(2), cursor.getDouble(3), cursor.getString(4))
-            registros.add(columnas)
-        }
-
-        val adaptador = AdaptadorPersonas(this, registros)
-        lstPastes.adapter = adaptador
-    }
-
-    internal class AdaptadorPersonas(context: Context, datos: List<Pastes>): ArrayAdapter<Pastes>(context, R.layout.item_paste_lista, datos)
-    {
-        private val imageConverter: ImageConverter = ImageConverter()
-        var _datos: List<Pastes>
-        init {
-            _datos = datos
-        }
-
-        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-                var inflater = convertView ?: LayoutInflater.from(context).inflate(R.layout.item_paste_lista, parent, false)
-
-                val Entidad = getItem(position)
-
-                inflater.lblTitulo2.text = Entidad!!._NombrePaste
-                inflater.lblSubTitulo2.text = Entidad!!._DescripcionPaste
-
-                //inflater.profile_image.setImageBitmap(imageConverter.bitmap(Entidad!!._imgPersona))
-
-                return inflater
-        }
-    }
 }
--- app/src/main/java/com/example/appfinalexamen/PasteCasa.kt
+++ app/src/main/java/com/example/appfinalexamen/PasteCasa.kt
@@ -8,12 +8,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
+import android.widget.ListView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.appfinalexamen.Entidades.Pastes
 import com.example.appfinalexamen.Modelo.ImageConverter
 import com.example.appfinalexamen.Modelo.PastesDB
 import com.example.appfinalexamen.R
 import kotlinx.android.synthetic.main.fragment_paste_casa.*
+import kotlinx.android.synthetic.main.item_lista.view.*
 import org.imaginativeworld.whynotimagecarousel.CarouselItem
 import org.imaginativeworld.whynotimagecarousel.ImageCarousel
 import java.lang.reflect.Field
@@ -87,7 +90,13 @@ class PasteCasa : Fragment() {
         }catch (ex:Exception){
 
         }*/
-        return inflater.inflate(R.layout.fragment_paste_casa, container, false)
+        val view: View =  inflater.inflate(R.layout.fragment_paste_casa, container, false)
+        val activity = activity as Context
+
+        val lstPastes = view.findViewById<ListView>(R.id.lstPastes)
+        LlenarInformacion(lstPastes)
+
+        return view
 
     }
 
@@ -106,6 +115,47 @@ class PasteCasa : Fragment() {
             PasteCasa().apply {
 
             }
+    }
+
+    fun LlenarInformacion(lstPastes: ListView){
+        val datasource = PastesDB(this.requireContext())
+
+        val registros = ArrayList<Pastes>()
+
+        val cursor = datasource.consultPastes()
+
+        while (cursor.moveToNext()){
+            val columnas = Pastes(cursor.getInt(0), cursor.getString(1), cursor.getString(2), cursor.getDouble(3), cursor.getString(4))
+            registros.add(columnas)
+        }
+
+        val adaptador = AdaptadorPersonas(this.requireContext(), registros)
+        lstPastes.adapter = adaptador
+    }
+
+    internal class AdaptadorPersonas(context: Context, datos: List<Pastes>): ArrayAdapter<Pastes>(context, R.layout.item_paste_lista, datos)
+    {
+        private val imageConverter: ImageConverter = ImageConverter()
+        var _datos: List<Pastes>
+        init {
+            _datos = datos
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            var inflater = convertView ?: LayoutInflater.from(context).inflate(R.layout.item_paste_lista, parent, false)
+
+            val Entidad = getItem(position)
+
+            val lblTitulo2 = inflater.findViewById<TextView>(R.id.lblTitulo)
+            val lblSubTitulo2 = inflater.findViewById<TextView>(R.id.lblSubTitulo)
+
+            lblTitulo2.text = Entidad!!._NombrePaste
+            lblSubTitulo2.text = Entidad!!._DescripcionPaste
+
+            //inflater.profile_image.setImageBitmap(imageConverter.bitmap(Entidad!!._imgPersona))
+
+            return inflater
+        }
     }
 
     fun AgregarPastes(view: View) {
